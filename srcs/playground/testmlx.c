3cfab7529564da645f882682e6c04df853b329aa
Morgan Roux <roux.morgan@gmail.com>
2020-02-03 16:40:32 +0100

    premier essai : affichage fenetre et loop clavier

--- srcs/playground/testmlx.c
+++ srcs/playground/testmlx.c
@@ -6,14 +6,29 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 13:48:35 by mroux             #+#    #+#             */
-/*   Updated: 2020/02/03 15:40:20 by mroux            ###   ########.fr       */
+/*   Updated: 2020/02/03 16:37:18 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include "libftprintf.h"
 
-int main(void)
+int		key_hook(int keycode,void *param)
 {
-    ft_printf("hello");
+	(void)param;
+	ft_printf("%d\n", keycode);
+	return 1;
+}
+
+int		main(void)
+{
+	void	*mlx_ptr;
+	void	*mlx_win;
+
+	if ((mlx_ptr = mlx_init()) == NULL)
+		return (0);
+	if ((mlx_win = mlx_new_window(mlx_ptr, 100, 100, "Hello")) == NULL)
+		return (0);
+	mlx_key_hook(mlx_win, &key_hook ,NULL);
+	mlx_loop(mlx_ptr);
 }
