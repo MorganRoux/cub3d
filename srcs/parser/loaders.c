5a3d08d7d281cfeca9e3a9d66d51ccac8d8b738f
Morgan Roux <roux.morgan@gmail.com>
2020-03-09 18:26:36 +0100

    add errors for ft_split_ex

--- srcs/parser/loaders.c
+++ srcs/parser/loaders.c
@@ -6,7 +6,7 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 11:02:45 by mroux             #+#    #+#             */
-/*   Updated: 2020/03/09 18:11:05 by mroux            ###   ########.fr       */
+/*   Updated: 2020/03/09 18:25:54 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		load_map_dimensions(t_game_engine *ge, char *line)
 	int		i;
 
 	param = ft_split(line, ' ');
-	if (check_dim_param(param) != OK)
+	if (param == NULL || check_dim_param(param) != OK)
 		return (ERROR_DIM);
 	ge->screen_w = ft_atoi(param[1]);
 	ge->screen_w = ge->screen_w > 2560 ? 2560 : ge->screen_w;
@@ -36,11 +36,10 @@ int		load_colors(t_game_engine *ge, char *line)
 	char	**param;
 	int		n;
 	int		i;
-	int		r;
 
 	param = ft_split_ex(line, ", ");
-	if ((r = check_color_param(param)) != OK)
-		return (r);
+	if (param == NULL || check_color_param(param) != OK)
+		return (ERROR_COLORS);
 	n = ft_strcmp(param[0], "F") == 0 ? 0 : 1;
 	ge->map.color[n].r = ft_atoi(param[1]);
 	ge->map.color[n].v = ft_atoi(param[2]);
@@ -57,12 +56,11 @@ int		load_sprite(t_game_engine *ge, char *line)
 	t_sprite	*sprite;
 	char		**param;
 	int			i;
-	int			r;
 	
 	i = 0;
 	param = ft_split(line, ' ');
-	if ((r = check_sprite_param(param)) != OK)
-		return (r);
+	if (param == NULL || check_sprite_param(param) != OK)
+		return (ERROR_SPRITE);
 	while (i < SPRITE_NUMBER)
 	{
 		sprite = &ge->map.sprite[i];
@@ -78,6 +76,33 @@ int		load_sprite(t_game_engine *ge, char *line)
 					&sprite->texture.size_line, &sprite->texture.endian);
 		i++;
 	}
+	i = 0;
+	while (param[i] != 0)
+		free(param[i++]);
+	free(param);
+	return (OK);
+}
+
+int		load_textures(t_game_engine *ge, char *line)
+{
+	t_img	*tex;
+	char	**param;
+	int		i;
+
+	param = ft_split(line, ' ');
+	if (param == NULL || check_tex_param(param) != OK)
+		return (ERROR_TEX);
+	tex = &ge->map.textures[get_tex_orientation(line[0])];
+	tex->path = param[1];
+	tex->p_img = mlx_xpm_file_to_image(
+					ge->mlx_ptr,
+					tex->path,
+					&tex->w, &tex->h);
+	if (tex->p_img == 0)
+		return (ERROR);
+	tex->data = mlx_get_data_addr(
+				tex->p_img, &tex->bits_per_pxl,
+				&tex->size_line, &tex->endian);
 	i = 0;
 	while (param[i] != 0)
 		free(param[i++]);
@@ -104,33 +129,5 @@ int		load_map(t_game_engine *ge, int fd, char *firstline)
 	ge->map.size = n;
 	free(line);
 	check_map(ge);
-	return (OK);
-}
-
-int		load_textures(t_game_engine *ge, char *line)
-{
-	t_img	*tex;
-	char	**param;
-	int		i;
-	int		r;
-
-	param = ft_split(line, ' ');
-	if ((r = check_tex_param(param)) != OK)
-		return (r);
-	tex = &ge->map.textures[get_tex_orientation(line[0])];
-	tex->path = param[1];
-	tex->p_img = mlx_xpm_file_to_image(
-					ge->mlx_ptr,
-					tex->path,
-					&tex->w, &tex->h);
-	if (tex->p_img == 0)
-		return (ERROR);
-	tex->data = mlx_get_data_addr(
-				tex->p_img, &tex->bits_per_pxl,
-				&tex->size_line, &tex->endian);
-	i = 0;
-	while (param[i] != 0)
-		free(param[i++]);
-	free(param);
 	return (OK);
 }
